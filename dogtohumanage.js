073daac8f1d8b58e8037ecaff207f131590beaa0
Sara Lutes <saralutes@gmail.com>
2022-01-06 13:51:00 -0600

    Create dogtohumanage.js

---
+++ dogtohumanage.js
@@ -0,0 +1,6 @@
+// 1 create a prompt to ask a user for thier dog's age
+var dogAge = prompt("Welcome to the dog to human age converter! Please enter your dog's age below:")
+// 2 Convert the dog's age to human age
+var humanAge = ((dogAge-2)*4)+21
+// 3 create an alert to tell the user thier dog's age in human years
+window.alert("Your dog is " + humanAge + " in human years!")
